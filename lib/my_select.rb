13133ae4616b5bbc3076bb1e46cf43aa5c1242da
Alexander Gutterman <alexanderf.gutterman@gmail.com>
2017-12-12 19:59:40 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
  # your code here!
+ i = 0
+ collection_array = []
+ while i < collection.length
+   selection = yield(collection[i])
+   case selection
+   when true
+     collection_array << collection[i]
+   end
+   i += 1
+ end
+ collection_array
 end
