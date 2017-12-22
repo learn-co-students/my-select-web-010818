2815d84548683541113473b7232277dd6bbe8e0c
github username <github email address>
2017-12-21 19:32:51 -0500

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,6 +1,33 @@
 def my_select(collection)
 
-  collection.select do |x|
-    x.even? 
+  new_Coll = []
+  i = 0
+
+  while i < collection.length
+    if yield(collection[i]) == true
+    new_Coll << collection[i]
+    end
+  i+=1
   end
+  new_Coll
 end
+
+
+#def my_select(collection)
+
+  #new_Coll = []
+  #i = 0
+
+  #while i < collection.length
+  #  new_Coll << yield(collection[i])
+  #  i+=1
+  #end
+  #new_Coll
+#end
+
+#with this code, i am returning
+#[false, true, false, true, false].
+#i need to return [2,4].
+#So i need to return the values that
+#  evalute to be true.
+#if number evalutes to true. store that number
