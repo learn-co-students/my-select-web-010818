71d9e4770c81bd5b8f9a5ccfd33fffa803527bf0
Christina Collura <cmcollura@gmail.com>
2018-01-04 20:15:15 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,17 +1,12 @@
-require "pry"
-
 def my_select(collection)
  if block_given?
    new_collection = []
    i = 0 
    while i < collection.length
-  binding.pry
-    i += 1
     if yield(collection[i]) 
-      binding.pry
      new_collection << collection[i]
-      
     end
+    i += 1
    end
    new_collection
  else
